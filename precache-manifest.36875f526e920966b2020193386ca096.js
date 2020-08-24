4916703c80b989b6daa8a737be32846f0c61ac30
saitejababu <saiteja.kalepu@gmail.com>
2020-08-24 17:13:02 +0530

    Updates

---
+++ precache-manifest.36875f526e920966b2020193386ca096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fb52a0843ed2343f3652fca3cc6ee5",
+    "url": "/Iphone-Calculator-React-App/index.html"
+  },
+  {
+    "revision": "7a387d1bbb7a6954f53d",
+    "url": "/Iphone-Calculator-React-App/static/css/main.ea448e6c.chunk.css"
+  },
+  {
+    "revision": "d51fce78b22c7714b9b6",
+    "url": "/Iphone-Calculator-React-App/static/js/2.dabea9ca.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Iphone-Calculator-React-App/static/js/2.dabea9ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a387d1bbb7a6954f53d",
+    "url": "/Iphone-Calculator-React-App/static/js/main.d164899f.chunk.js"
+  },
+  {
+    "revision": "19357795e64fef4e5396",
+    "url": "/Iphone-Calculator-React-App/static/js/runtime-main.d7e52a04.js"
+  }
+]);
